265ef027f15b2f9a50ce31f89683668418b59d7b
pareshsutariya <paresh.sutariya.it@gmail.com>
2021-01-07 20:32:29 -0500

    first commit

---
+++ src/main/java/net/xitiz/jrd/JrdApplication.java
@@ -0,0 +1,16 @@
+package net.xitiz.jrd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JrdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JrdApplication.class, args);
+
+		System.out.println("====== Welcome =======");
+		System.out.println("====== Hello....from Java World !! =======");
+	}
+
+}
---
+++ src/test/java/net/xitiz/jrd/JrdApplicationTests.java
@@ -0,0 +1,13 @@
+package net.xitiz.jrd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JrdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
