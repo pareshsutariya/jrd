17b2e4bd6df540223a5e085a1abd58f44bdbe1cd
pareshsutariya <76577398+pareshsutariya@users.noreply.github.com>
2021-01-08 23:59:05 -0500

    Update AlterationRepositoryTests.java

--- src/test/java/net/xitiz/jrd/AlterationRepositoryTests.java
+++ src/test/java/net/xitiz/jrd/AlterationRepositoryTests.java
@@ -3,13 +3,15 @@ package net.xitiz.jrd;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import net.xitiz.jrd.repositories.AlterationRepository;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = JrdApplication.class)
 public class AlterationRepositoryTests {
 
@@ -19,8 +21,8 @@ public class AlterationRepositoryTests {
     @Test
     void findById() {
         System.out.println("======== Test Running ====== ");
-        var item = repo.findById(1);
-        assertEquals(true, item.isPresent());
+        var item = repo.findById(2);
+        assertEquals("Alteration-2", item.get().getName());
     }
 
 }
