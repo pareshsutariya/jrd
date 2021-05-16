701f1ad7883339614130bc1fd3e4063db84f6126
pareshsutariya <paresh.sutariya.it@gmail.com>
2021-05-16 15:45:20 -0400

    adding CrossOrigin

--- src/main/java/net/xitiz/jrd/rest/AlterationRestController.java
+++ src/main/java/net/xitiz/jrd/rest/AlterationRestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import net.xitiz.jrd.entities.*;
 import net.xitiz.jrd.exceptions.AlterationNotFoundException;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/alterations")
 public class AlterationRestController {
 
--- src/main/java/net/xitiz/jrd/rest/TicketRestController.java
+++ src/main/java/net/xitiz/jrd/rest/TicketRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import net.xitiz.jrd.entities.Ticket;
 import net.xitiz.jrd.repositories.TicketRepository;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/tickets")
 public class TicketRestController {
 
