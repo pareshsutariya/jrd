f466fb6b85484af072ee80423f96689aaf6d7b63
pareshsutariya <paresh.sutariya.it@gmail.com>
2021-05-23 12:33:06 -0400

    server.error.include-stacktrace

--- src/main/java/net/xitiz/jrd/exceptions/AlterationNotFoundException.java
+++ src/main/java/net/xitiz/jrd/exceptions/AlterationNotFoundException.java
@@ -1,5 +1,9 @@
 package net.xitiz.jrd.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)  
 public class AlterationNotFoundException extends RuntimeException {
     public AlterationNotFoundException(String message) {
         super(message);
